12a2e916775de8d052617c2c21ea45631a6f2d03
1322737886 <1322737886@qq.com>
2020-11-12 11:25:06 +0800

    11.12

--- app/src/main/java/com/nbpt/zyx_sb/Main4Activity.java
+++ app/src/main/java/com/nbpt/zyx_sb/Main4Activity.java
@@ -2,21 +2,55 @@ package com.nbpt.zyx_sb;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main4Activity extends Activity {
 
         String Fruitname[] = {"苹果","香蕉","樱桃","葡萄","芒果","橘子","梨","菠萝","草莓","西瓜"};
         GridView gridView;
+    int image[]= {R.drawable.apple_pic,R.drawable.banana_pic,
+                    R.drawable.cherry_pic,R.drawable.grape_pic,
+                    R.drawable.mango_pic,R.drawable.orange_pic,
+                    R.drawable.pear_pic,R.drawable.pineapple_pic,
+                    R.drawable.strawberry_pic,R.drawable.watermelon_pic};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
 
         GridView gridView = (GridView)findViewById(R.id.pl);
-        ArrayAdapter<String>adapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,Fruitname);
 
+        String[] from = {"img","name"};
+        int[] to ={R.id.image1,R.id.textV1};
+        List<Map<String,Object>> imageList= new ArrayList<Map<String,Object>>();
+        for(int i=0;i<image.length;i++){
+            Map<String,Object> map = new HashMap<String, Object>();
+            map.put("img",image[i]);
+            map.put("name",Fruitname[i]);
+            imageList.add(map);
+        }
+        SimpleAdapter adapter= new SimpleAdapter(Main4Activity.this,imageList,R.layout.zxc,from,to);
         gridView.setAdapter(adapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(Main4Activity.this,"当前点击的水果是："+Fruitname[i],Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//        ArrayAdapter<String>adapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,Fruitname);
+//        gridView.setAdapter(adapter);
     }
 }
